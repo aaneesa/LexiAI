1f06802aff77b67d4e30a25ea99d8dc8af02d906
Anwesha Adhikari <anweshaa002@gmail.com>
2026-01-01 23:52:58 +0530

    backend folder structure (#1)
    This PR contains necessary backend folder structure

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import errorHandler from './middleware/errorHandler.js';
+import connectDB from './config/db.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+connectDB();
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use(errorHandler);
+app.use((req,res)=>{
+    res.status(404).json({ message: 'Route not found' });
+})
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
