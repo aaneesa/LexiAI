427e326f468d41dce43cf6623ab7e7b6dedb9b3b
Anwesha Adhikari <anweshaa002@gmail.com>
2026-01-06 10:18:18 +0530

    Flashcard apis (untested) (#10)
    * backend folder structure
    
    * setting up mongodb connection
    
    * auth apis creation and test pass
    
    * quiz and flaashcard schema added
    
    * Remove node_modules from repository and ignore it
    
    * Remove .env from Git tracking
    
    * Document and ChatHistory models created
    
    * working auth apis
    
    * working document apis
    
    * implement flashcard APIs (untested)

---
+++ server/routes/flashcardRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import {
+    getFlashcards,
+    getAllFlashcards,
+    reviewFlashcards,
+    toggleStarFlashcard,
+    deleteFlashcard
+} from '../controllers/flashcardController.js';
+import {protect} from '../middlewares/auth.js';
+
+const router = express.Router();
+
+router.use(protect);
+
+router.get('/',getAllFlashcards)
+router.get('/:documentId',getFlashcards);
+router.post('/:flashcardId/review',reviewFlashcards);
+router.post('/:flashcardId/toggle-star',toggleStarFlashcard);
+router.delete('/:flashcardId',deleteFlashcard);
+
+export default router;
