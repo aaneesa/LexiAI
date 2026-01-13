ac4f0de54cb2243a2190aa8b07a13b4f85f645d2
Anwesha Adhikari <anweshaa002@gmail.com>
2026-01-13 22:43:28 +0530

    Added Auth context (initialize only) (#21)
    * backend folder structure
    
    * setting up mongodb connection
    
    * auth apis creation and test pass
    
    * quiz and flaashcard schema added
    
    * Remove node_modules from repository and ignore it
    
    * Remove .env from Git tracking
    
    * Document and ChatHistory models created
    
    * working auth apis
    
    * working document apis
    
    * implement flashcard APIs (untested)
    
    * built ai-apis and tested flashcard-generator api
    
    * tested ai and flashcard apis
    
    * fixed textchunker
    
    * working and tested quiz-apis
    
    * working api for dashboard
    
    * rrender route not found resolved
    
    * added authServices and fix typo in authController
    
    * defined apiPaths and authservices
    
    * Remove node_modules from repository
    
    * defined all the other api services
    
    * remaining apiServices
    
    * Added context

---
+++ client/lexiai/src/context/AuthProvider.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { AuthContext } from "./AuthContext";
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    checkAuthStatus();
+  }, []);
+
+  const login = (userData, token) => {
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(userData));
+    setUser(userData);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setUser(null);
+    setIsAuthenticated(false);
+    window.location.href = "/";
+  };
+
+  const updateUser = (updatedUserData) => {
+    const newUserData = { ...user, ...updatedUserData };
+    localStorage.setItem("user", JSON.stringify(newUserData));
+    setUser(newUserData);
+  };
+
+  const checkAuthStatus = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const userStr = localStorage.getItem("user");
+
+      if (token && userStr) {
+        setUser(JSON.parse(userStr));
+        setIsAuthenticated(true);
+      }
+    } catch {
+      logout();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        loading,
+        isAuthenticated,
+        login,
+        logout,
+        updateUser,
+        checkAuthStatus,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
